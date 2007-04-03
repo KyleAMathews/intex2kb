2c5046ce129641037ae4d7f460790cda83e5538b
tylerfarmer <tylerfarmer@f1e1b38a-2c2d-0410-91e0-9f41c60f0189>
2007-04-03 15:57:58 +0000

    ""

--- trunk/Intex2kb/src/edu/byu/isys413/cbb54/intex2kb/data/printOrder.java
+++ trunk/Intex2kb/src/edu/byu/isys413/cbb54/intex2kb/data/printOrder.java
@@ -10,8 +10,8 @@
 package edu.byu.isys413.cbb54.intex2kb.data;
 
 /**
- *
- * @author kyle
+ * PrintOrder is a one of several possible revenue sources for the MyStuff system.  It is the printing of pictures either digital or film.
+ * @author tyler
  */
 public class printOrder extends RevenueSource {
     
@@ -20,42 +20,77 @@ public class printOrder extends RevenueSource {
     private int quantity;
     private String poID;
     
-    /** Creates a new instance of printOrder */
+    /**
+     * Creates a new instance of printOrder
+     * @param id GUID
+     */
     public printOrder(String id) {
         this.id = id;
     }
 
+    /**
+     * Return photoset
+     * @return photoset
+     */
     public PhotoSet getPhotoSet() {
         return photoSet;
     }
 
+    /**
+     * Set PhotoSet object
+     * @param photoSet Photoset associated with printorder
+     */
     public void setPhotoSet(PhotoSet photoSet) {
         this.photoSet = photoSet;
         this.dirty = true;
     }
 
+    /**
+     * Get Quantity
+     * @return Number of orders of a particular type & photoset
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * Set Quantity
+     * @param quantity Number of a particular photoset and printformat
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
         this.dirty = true;
     }
 
+    /**
+     * Get PrintFormat
+     * @return Papertype, size, sourcetype
+     */
     public printFormat getPrintFormat() {
         return printFormat;
     }
 
+    /**
+     * setPrintFormat
+     * @param printFormat Papertype, size, sourcetype
+     */
     public void setPrintFormat(printFormat printFormat) {
         this.printFormat = printFormat;
         this.dirty = true;
     }
 
+    /**
+     * get PrintOrderID
+     * @return return PrintOrderID
+     */
     public String getPoID() {
         return poID;
     }
 
+    /**
+     * set PrintOrderID
+     * @param poID PrintOrderID
+     */
     public void setPOID(String poID) {
         this.poID = poID;
         this.dirty = true;
