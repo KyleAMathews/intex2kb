2af06eddea56f6dd8aa07615a9012aa79bc714c7
tylerfarmer <tylerfarmer@f1e1b38a-2c2d-0410-91e0-9f41c60f0189>
2007-04-09 21:18:47 +0000

    ""

---
+++ Intex2kb/src/edu/byu/isys413/cbb54/intex2kb/data/sendMail.java
@@ -0,0 +1,28 @@
+/*
+ * sendMail.java
+ *
+ * Created on April 9, 2007, 2:11 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ *
+ * User: info@2kmystuff.com
+ * Pass: intex24
+ * POP: pop.1and1.com
+ * SMTP: smtp.1and1.com
+ */
+
+package edu.byu.isys413.cbb54.intex2kb.data;
+
+/**
+ *
+ * @author tylerf
+ */
+public class sendMail {
+    
+    /** Creates a new instance of sendMail */
+    public sendMail(String to, String body) {
+        
+    }
+    
+}
