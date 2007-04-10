5eb868945b08cf138780b56d0fbe0655917cc917
tylerfarmer <tylerfarmer@f1e1b38a-2c2d-0410-91e0-9f41c60f0189>
2007-04-10 00:30:05 +0000

    ""

--- trunk/Intex2kb/src/edu/byu/isys413/cbb54/intex2kb/data/printConvBatch.java
+++ trunk/Intex2kb/src/edu/byu/isys413/cbb54/intex2kb/data/printConvBatch.java
@@ -39,19 +39,23 @@ public class printConvBatch {
     }
     
     //Find all tx with print or conversion order and status complete & return customer e-mail
-    public List<Customer> getReady() throws Exception {
-        List l = new LinkedList<Customer>();
+    public void getReady() throws Exception {
+        List l = new LinkedList<String>();
         
         //query db for txs
         Connection conn = ConnectionPool.getInstance().get();
-        PreparedStatement ps = conn.prepareStatement("select * from \"transactionline\" where \"rstype\" = '" +
-                "conv' or \"rstype\" = 'po'");
+        PreparedStatement ps = conn.prepareStatement("select distinct custid from \"transaction\" where exists" +
+                " (select * from transactionline where \"rstype\" like '" +
+                "Conversion%' or \"rstype\" like 'Print Order%')");
         ResultSet rs = ps.executeQuery();
         while(rs.next()){
-            Transaction tx = TransactionDAO.getInstance().read(rs.getString("transactionid"));
-            l.add(tx.getCustomer());
+            Customer cust = CustomerDAO.getInstance().read(rs.getString("custid"),conn);
+            l.add(cust.getEmail());
         }
         
-        return l;
+        sendMail sm = new sendMail();
+        sm.postMail(l,"The order that was received by MyStuff Digital Photography " +
+                "is now ready to be picked up.  Please come as soon as possible to " +
+                "claim your order.");
     }
 }
