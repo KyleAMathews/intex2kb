170d1a6023b243238469c1f6c47d80ca9dac139e
mathews.kyle <mathews.kyle@f1e1b38a-2c2d-0410-91e0-9f41c60f0189>
2007-04-12 16:45:03 +0000

    ""

---
+++ trunk/MyStuffWeb/src/java/edu/byu/isys413/actions/backuptx.java
@@ -0,0 +1,47 @@
+/*
+ * backuptx.java
+ *
+ * Created on April 12, 2007, 9:37 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package edu.byu.isys413.actions;
+
+import edu.byu.isys413.cbb54.intex2kb.data.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author kyle
+ */
+public class backuptx implements edu.byu.isys413.web.Action{
+    
+    private Transaction tx;
+    private Double GBs;
+    private backup bck;
+    /** Creates a new instance of backuptx */
+    public backuptx() {
+    }
+    
+    public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        HttpSession session = request.getSession();
+        
+        //retrieve transaction
+        tx = TransactionDAO.getInstance().read((String)session.getAttribute("backuptx"));
+        
+        // grab how many gbs and set on revenue source
+        GBs = Double.valueOf(request.getParameter("numGBs"));
+        bck = (backup)tx.getTxLines().get(0).getRevenueSource();
+        bck.setSize(GBs);
+        
+        // calc subtotal
+        bck.calcSubTotal();
+        
+        return "backuptx.jsp";
+    }
+    
+}
